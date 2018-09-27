61d88f0a3410a6fc40291d1df69e21b8af37351f
mkeley <43392279+mkeley@users.noreply.github.com>
2018-09-26 22:23:31 -0400

    Add files via upload

---
+++ calc.rb
@@ -0,0 +1,39 @@
+# output an integer
+
+puts 3 
+
+# output the number of seconds in a week
+
+puts 60*(60*24)*7
+
+# output a float
+
+puts 98.999
+
+# output a string
+
+puts 'I like ' + 'Pepperoni Pizza.'
+
+
+# output concatination of strings
+
+String1 = "Hello"
+
+String2 = "World"
+
+puts String1  + String2
+
+str1 = 'Hello'
+
+str2 = 'World'
+
+puts str1 + str2
+
+
+
+
+
+
+
+
+
